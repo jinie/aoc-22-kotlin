721e8ea5043a3c7b33516116d36828c82797114e
Jimmy Selgen Nielsen <jin@bec.dk>
2022-12-01 14:37:44 +0100

    Day 1 solution Refactored.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,33 +1,26 @@
-fun main() {
+fun getList(input: List<String>): HashMap<Int, Int> {
+    var idx = 0
+    var sum = 0
+    val sums = HashMap<Int, Int>()
 
-    fun getList(input: List<String>): HashMap<Int, Int> {
-        var idx = 0
-        var sum = 0
-        val sums = HashMap<Int, Int>()
-
-        input.map { it.trim() }.forEach {
-            if (it.isEmpty()) {
-                sums[idx++] = sum
-                sum = 0
-            } else {
-                sum += it.toInt()
-            }
+    input.map { it.trim() }.forEach {
+        if (it.isNotEmpty()) {
+            sum += it.toInt()
+        } else {
+            sums[idx++] = sum
+            sum = 0
         }
-        return sums
     }
+    return sums
+}
 
-    fun part1(input: List<String>): Int {
-        return getList(input).values.sortedDescending()[0]
-    }
+fun part1(input: List<String>): Int = getList(input).values.sortedDescending()[0]
+fun part2(input: List<String>): Int = getList(input).values.sortedDescending().subList(0, 3).sum()
 
-    fun part2(input: List<String>): Int {
-        val sums = getList(input)
-        return sums.values.sortedDescending().subList(0, 3).sum()
-    }
-
-    // test if implementation meets criteria from the description, like:
+fun main() {
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 24000)
+
     measureTimeMillisPrint {
         val input = readInput("Day01")
         println(part1(input))
