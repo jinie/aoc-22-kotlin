9b4ad42d78d025e8d61f497281d750af9f844d1a
Jimmy Selgen Nielsen <jin@bec.dk>
2022-12-01 11:24:38 +0100

    Day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,36 @@
 fun main() {
+
+    fun getList(input: List<String>): HashMap<Int, Int> {
+        var idx = 0
+        var sum = 0
+        val sums = HashMap<Int, Int>()
+
+        input.map { it.trim() }.forEach {
+            if (it.isEmpty()) {
+                sums[idx++] = sum
+                sum = 0
+            } else {
+                sum += it.toInt()
+            }
+        }
+        return sums
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return getList(input).values.sortedDescending()[0]
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val sums = getList(input)
+        return sums.values.sortedDescending().subList(0, 3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    check(part1(testInput) == 24000)
+    measureTimeMillisPrint {
+        val input = readInput("Day01")
+        println(part1(input))
+        println(part2(input))
+    }
 }
