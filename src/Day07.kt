ba39bbc44a17444b3381ccaad27f3d48aa816ef4
Jimmy Selgen Nielsen <jimmy.selgen@gmail.com>
2022-12-09 09:40:24 +0100

    Day 07 Solution

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,5 +1,3 @@
-import java.io.File
-import java.util.*
 class Day07 {
     fun parseInput(input: List<String>): Map<String, Int>{
         val currentDir = StringBuilder()
@@ -21,24 +19,52 @@ class Day07 {
                 }
                 i++
             }else{
-                var j = i
                 val files: MutableList<String> = mutableListOf()
-                while(++j < input.size && !input[j].startsWith("$")) {
-                    files.add(input[j++])
+                while(++i < input.size){
+                    if(input[i].startsWith("$"))
+                        break
+                    files.add(input[i])
                 }
                 dirTree[currentDir.toString()] = files
                     .filter { it.startsWith("dir").not() }
                     .fold(initial = 0) { acc, str -> acc + str.split(" ").first().toInt() }
-                i=j-1
             }
         }
-        return dirTree
+        return calculateDirectorySizes(dirTree)
+    }
+
+    private fun calculateDirectorySizes(dirTree: Map<String,Int>): Map<String,Int>{
+        val sizedTree = mutableMapOf<String,Int>()
+        dirTree.entries.forEach{ 
+            var curDir = it.key
+            while(curDir.isNotEmpty()){
+                curDir = curDir.dropLast(1)
+                val key = curDir.ifBlank { "/" }
+                sizedTree.merge(key, it.value) { a,b -> a+b }
+                curDir = curDir.dropLastWhile { itt -> itt != '/' }
+            }
+        }
+        return sizedTree
+    }
+
+    fun part1(dirMap: Map<String,Int>): Int{
+        return dirMap.values.filter { it <= 100000 }.sum()
+    }
+
+    fun part2(dirMap: Map<String, Int>): Int {
+        return dirMap.values.groupBy { it > (30000000 - (70000000 - dirMap["/"]!!)) }[true]!!.min()
     }
 }
 
 fun main() {
+    val d7 = Day07()
     val testInput = readInput("Day07_test")
-    val res = Day07().parseInput(testInput)
-    println(res["/"])
-    check(Day07().parseInput(testInput)["/"] == 48381165)
+    val test = d7.parseInput(testInput)
+    check(d7.part1(test) == 95437)
+
+    measureTimeMillisPrint {
+        val tree = d7.parseInput(readInput("Day07"))
+        println(d7.part1(tree))
+        println(d7.part2(tree))
+    }
 }
